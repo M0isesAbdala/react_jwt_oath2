787be80a634e55def16947698e95661f4a58c44d
M0isesAbdala <moises.abdala@outlook.com>
2026-05-19 22:22:52 -0300

    vite.config env

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    allowedHosts: [
-      'mysite.com'
-    ]
-  },
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const ENV = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: parseInt(ENV.VITE_PORT) || 5173,
+      strictPort: true,
+      allowedHosts: [
+        'mysite.com'
+      ]
+    },
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+  };
+});
